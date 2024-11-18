5a13a4c79bc0d28681150eacaafb3d707438cd59
のん <160106334+nonniya@users.noreply.github.com>
2024-11-18 15:47:39 +0900

    Update hanakotoba.js

--- hanakotoba.js
+++ hanakotoba.js
@@ -12,14 +12,27 @@ assessmentButton.addEventListener(
         }
         
         resultDivision.innerText = '';
-        const hedding = document.createElement('h3');
-        hedding.innerText = '診断結果';
-        resultDivision.appendChild(hedding);
+        // headerDivision の作成
+        const headerDivision = document.createElement('div');
+        headerDivision.setAttribute('class', 'card-header text-bg-primary');
+        headerDivision.innerText = '診断結果';
+
+        // bodyDivision の作成
+        const bodyDivision = document.createElement('div');
+        bodyDivision.setAttribute('class', 'card-body');
 
         const paragraph = document.createElement('p');
+        paragraph.setAttribute('class', 'card-text');
         const result = assessment(userName);
-        paragraph.innerText =result;
-        resultDivision.appendChild(paragraph);
+        paragraph.innerText = result;
+        bodyDivision.appendChild(paragraph);
+
+        // resultDivision に Bootstrap のスタイルを適用する
+        resultDivision.setAttribute('class', 'card');
+
+        // headerDivision と bodyDivision を resultDivision に差し込む
+        resultDivision.appendChild(headerDivision);
+        resultDivision.appendChild(bodyDivision);
     }
 );
 
